7043e6a60ddfb1dfeee5c0a22e42fc0cd3ef991f
avanegasp <arco527@gmail.com>
2024-05-20 15:22:04 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+// Componente Likes
+
+const Likes = () => {
+  const [likes, setLikes] = useState();
+
+  const increase = () => {
+    setLikes(likes + 1);
+  };
+  const decrease = () => {
+    setLikes(likes - 1);
+  };
+  const reset = () => setLikes(0);
+
+  return {likes, increase, decrease, reset};
+};
+
+const App = () => {
+  const { likes, increase, decrease, reset } = Likes();
+  return (
+    <div>
+      <p>Estos son los likes {likes}</p>
+      <button onClick={increase}>Suma</button>
+      <button onClick={decrease}>Resta</button>
+      <button onClick={reset}>Comienza</button>
+    </div>
+  );
+};
+
+export default App;
